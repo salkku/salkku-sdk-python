54957cfc737f7051e9c4e335a3ca886ed75bb6a5
Sami Tikka <stikka@iki.fi>
2016-06-21 16:27:38 +0300

    add sdk code and documentation

---
+++ src/salkku-cli.py
@@ -0,0 +1,94 @@
+import argparse
+import sdkhttp
+
+DEFAULT_HOST = "https://salkku.co"
+
+options = {
+    'verbose': False
+}
+
+
+def parse_command_line():
+    # type: () -> Namespace
+    parser = argparse.ArgumentParser(description='CLI commands for paaomat.fi.')
+    parser.add_argument('command', help='Main command name')
+    parser.add_argument('--data', help='Post data file')
+    parser.add_argument('--host', help='Salkku host address', default=DEFAULT_HOST)
+    parser.add_argument('--user', help='HTTP auth user')
+    parser.add_argument('--password', help='HTTP auth password')
+    parser.add_argument('--id', help='Optional resource id', type=int)
+    parser.add_argument('--email', help='Optional user email')
+    parser.add_argument('--date', help='Begin date')
+    parser.add_argument('--name', help='Optional resource name')
+    parser.add_argument('--format', help='API response format')
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        dest="verbose", help='Verbose output')
+    parser.add_argument('-d', '--dev', action='store_true',
+                        dest="debug", help='Development mode')
+
+    args = parser.parse_args()
+
+    return args
+
+
+def main():
+    args = parse_command_line()
+
+    if args.verbose:
+        options['verbose'] = True
+    if args.debug:
+        options['verify'] = False
+    else:
+        options['verify'] = True
+
+    api_url = '{}/api/v1'.format(args.host)
+    client = sdkhttp.HTTPClient(api_url, args.user, args.password, options)
+
+    if args.command == "currency":
+        stdout(client.get_currencies())
+    elif args.command == "exchange":
+        stdout(client.get_exchanges())
+    elif args.command == "exchange-security":
+        if args.id is not None:
+            stdout(client.get_exchange_securities(args.id))
+        else:
+            raise Exception("Need exchange id")
+    elif args.command == "security":
+        if args.name is not None:
+            stdout(client.search_security(args.name))
+        elif args.id is not None:
+            stdout(client.get_security(args.id))
+    elif args.command == "portfolio":
+        if args.id is not None:
+            stdout(client.get_portfolio(args.id))
+        else:
+            stdout(client.get_portfolios())
+    elif args.command == "portfolio-history":
+        if args.id is not None:
+            stdout(client.get_portfolio_history(args.id))
+        else:
+            raise Exception("Need portfolio id")
+    elif args.command == "portfolio-performance":
+        if args.id is not None:
+            stdout(client.get_portfolio_performance(args.id))
+        else:
+            raise Exception("Need portfolio id")
+    elif args.command == "ping":
+        stdout(client.ping())
+    elif args.command == "portfolio-transaction":
+        if args.id is not None:
+            stdout(client.get_portfolio_transactions(args.id, args.format))
+        else:
+            raise Exception("Need portfolio id")
+    elif args.command == "portfolio-dividend":
+        if args.id is not None:
+            stdout(client.get_portfolio_dividends(args.id, args.format))
+        else:
+            raise Exception("Need portfolio id")
+
+
+def stdout(response):
+    print(response.text)
+
+if __name__ == "__main__":
+    main()
---
+++ src/sdkhttp.py
@@ -0,0 +1,188 @@
+import requests
+import json
+from requests.auth import HTTPBasicAuth
+
+
+class HTTPClient:
+
+    def __init__(self, api_uri, user, password, options):
+        # type: (string, string, string, Dict[str, bool]) -> HTTPClient
+
+        self.api_uri = api_uri
+        self.user = user
+        self.password = password
+        self.options = options
+
+    def __make_authenticated_get_request(self, url, payload=None):
+        # type: (string, Dict) -> requests.Response
+        if payload is None:
+            payload = {}
+        auth_token = self.authenticate()
+
+        response = requests.get(
+            url,
+            headers={
+                'salkku-auth-token': auth_token
+            },
+            params=payload,
+            verify=self.options['verify'],
+            allow_redirects=False
+        )
+        if self.options['verbose']:
+            print("GET {}".format(response.url))
+        if response.status_code != requests.codes.ok:
+            if self.options['verbose']:
+                print(response.text)
+            response.raise_for_status()
+
+        return response
+
+    def __make_authenticated_post_request(self, url, post_data=None, auth_token=None, headers=None):
+        # type: (string, Dict, string, Dict) -> requests.Response
+        if headers is None:
+            headers = {}
+        if post_data is None:
+            post_data = {}
+        if auth_token is None:
+            auth_token = self.authenticate()
+
+        headers['salkku-auth-token'] = auth_token
+        response = requests.post(
+            url,
+            headers=headers,
+            data=post_data,
+            verify=self.options['verify'],
+            allow_redirects=False
+        )
+#        print(post_data)
+        if self.options['verbose']:
+            print("POST {}".format(response.url))
+        if response.status_code != requests.codes.ok:
+            if self.options['verbose']:
+                print(response.text)
+            response.raise_for_status()
+
+        return response
+
+    def __post_json(self, url, post_data, auth_token=None, headers=None):
+        if headers is None:
+            headers = {}
+        headers['Content-Type'] = 'application/json'
+        return self.__make_authenticated_post_request(url, json.dumps(post_data), auth_token, headers)
+
+    def authenticate(self):
+        # type: () -> str
+        url = '{}/login'.format(self.api_uri)
+
+        response = requests.post(
+            url,
+            auth=HTTPBasicAuth(self.user, self.password),
+            verify=self.options['verify'],
+            allow_redirects=False
+        )
+        if self.options['verbose']:
+            print("POST {}".format(response.url))
+
+        auth_token = ""
+        if response.status_code == requests.codes.ok:
+            auth_token = response.headers.get('salkku-auth-token')
+        else:
+            if self.options['verbose']:
+                print(response.text)
+            response.raise_for_status()
+
+        return auth_token
+
+    def ping(self):
+        url = '{}/ping'.format(self.api_uri)
+        return self.__make_authenticated_get_request(url)
+
+    def get_currencies(self):
+        if self.options['verbose']:
+            print("Getting list of currencies...")
+
+        url = '{}/currency'.format(self.api_uri)
+        return self.__make_authenticated_get_request(url)
+
+    def get_exchanges(self):
+        if self.options['verbose']:
+            print("Getting list of exchanges...")
+
+        url = '{}/exchange'.format(self.api_uri)
+        return self.__make_authenticated_get_request(url)
+
+    def get_exchange_securities(self, exchange_id):
+        if self.options['verbose']:
+            print("Getting list of exchange {} securities...".format(exchange_id))
+
+        url = '{}/exchange/{}/security'.format(self.api_uri, exchange_id)
+        return self.__make_authenticated_get_request(url)
+
+    def search_security(self, search):
+        if self.options['verbose']:
+            print("Searching security with \"{}\"...".format(search))
+        params = {"q": search}
+
+        url = '{}/security'.format(self.api_uri)
+        return self.__make_authenticated_get_request(url, params)
+
+    def get_security(self, security_id):
+        if self.options['verbose']:
+            print("Get security with id \"{}\"...".format(security_id))
+
+        url = '{}/security/{}'.format(self.api_uri, security_id)
+        return self.__make_authenticated_get_request(url)
+
+    def get_portfolios(self):
+        # type: (string, string) -> requests.Response
+        if self.options['verbose']:
+            print("Getting all portfolios {}...")
+
+        url = '{}/portfolio'.format(self.api_uri)
+        return self.__make_authenticated_get_request(url)
+
+    def get_portfolio(self, portfolio_id):
+        # type: (string, string) -> requests.Response
+        if self.options['verbose']:
+            print("Getting portfolio with id {}...".format(portfolio_id))
+
+        url = '{}/portfolio/{}'.format(self.api_uri, portfolio_id)
+        return self.__make_authenticated_get_request(url)
+
+    def get_portfolio_history(self, portfolio_id):
+        # type: (string, string) -> requests.Response
+        if self.options['verbose']:
+            print("Getting history of portfolio {}...".format(portfolio_id))
+
+        url = '{}/portfolio/{}/history'.format(self.api_uri, portfolio_id)
+        return self.__make_authenticated_get_request(url)
+
+    def get_portfolio_performance(self, portfolio_id):
+        # type: (string, string) -> requests.Response
+        if self.options['verbose']:
+            print("Getting performance of portfolio {}...".format(portfolio_id))
+
+        url = '{}/portfolio/{}/performance'.format(self.api_uri, portfolio_id)
+        return self.__make_authenticated_get_request(url)
+
+    def get_portfolio_transactions(self, portfolio_id, api_format):
+        # type: (string, string) -> requests.Response
+        if self.options['verbose']:
+            print("Getting transactions of portfolio {}...".format(portfolio_id))
+
+        if api_format is not None:
+            url = '{}/portfolio/{}/transaction?format={}'.format(self.api_uri, portfolio_id, api_format)
+        else:
+            url = '{}/portfolio/{}/transaction'.format(self.api_uri, portfolio_id)
+        return self.__make_authenticated_get_request(url)
+
+    def get_portfolio_dividends(self, portfolio_id, api_format):
+        # type: (string, string) -> requests.Response
+        if self.options['verbose']:
+            print("Getting dividends of portfolio {}...".format(portfolio_id))
+
+        if api_format is not None:
+            url = '{}/portfolio/{}/dividend?format={}'.format(self.api_uri, portfolio_id, api_format)
+        else:
+            url = '{}/portfolio/{}/dividend'.format(self.api_uri, portfolio_id)
+        return self.__make_authenticated_get_request(url)
